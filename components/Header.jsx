f09d1663d5eb4d703d97eaa960b80e78287122c2
digiindia2025 <amantiwariat2005@gmail.com>
2025-03-27 14:20:18 +0530

    esme hmne logo ke size ko badha diya hai 45px md height

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
-
 import Link from "next/link";
 import Menu from "./Menu";
 import MenuMobile from "./MenuMobile";
-
-
 import { IoMdHeartEmpty } from "react-icons/io";
 import { BiMenuAltRight } from "react-icons/bi";
 import { VscChromeClose } from "react-icons/vsc";
-// import { fetchDataFromApi } from "@/utils/api"; // Commented out API import
 import { useSelector } from "react-redux";
 
 const Header = () => {
@@ -24,7 +20,7 @@ const Header = () => {
             if (window.scrollY > lastScrollY && !mobileMenu) {
                 setShow("-translate-y-[80px]");
             } else {
-                setShow("shadow-sm");
+                setShow("shadow-lg bg-white/90 backdrop-blur-md");
             }
         } else {
             setShow("translate-y-0");
@@ -41,11 +37,11 @@ const Header = () => {
 
     return (
         <header
-            className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+            className={`w-full h-[70px] md:h-[100px] bg-white flex items-center justify-between z-30 sticky top-0 transition-transform duration-300 shadow-md ${show}`}
         >
-            <Wrapper className="h-[60px] flex justify-between items-center">
+            <Wrapper className="h-[100px] flex justify-between items-center px-4 md:px-8">
                 <Link href="/">
-                    <img src="/images/logo.webp" className="w-[60px] md:w-[80px]" />
+                    <img src="/images/logo.webp" className="w-[120px] md:w-[130px] h-[60px] md:h-[45px]" />
                 </Link>
 
                 <Menu
@@ -64,30 +60,16 @@ const Header = () => {
                 )}
 
                 <div className="flex items-center gap-2 text-black">
-                    {/* Icon start */}
-                    {/* <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-                        <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
-                        <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-                            0
+                    <Link href="/login">
+                        <div className="w-20 md:w-24 h-8 md:h-10 flex justify-center items-center 
+                                        text-white bg-gradient-to-r from-blue-500 to-blue-700 
+                                        hover:from-blue-600 hover:to-blue-800 
+                                        rounded-full shadow-md text-[13px] md:text-[15px] font-semibold 
+                                        transition-all duration-300 cursor-pointer 
+                                        ml-[-10px] md:ml-[3px]">
+                            Login
                         </div>
-                    </div> */}
-                    {/* Icon end */}
-
-                   
-{/* Login Option Start */}
-<Link href="/login">
-    <div className="w-20 md:w-24 h-8 md:h-10 flex justify-center items-center 
-                    text-white bg-gradient-to-r from-blue-500 to-blue-700 
-                    hover:from-blue-600 hover:to-blue-800 
-                    rounded-full shadow-md text-[13px] md:text-[15px] font-semibold 
-                    transition-all duration-300 cursor-pointer 
-                    ml-[-10px] md:ml-[3px]">
-        Login
-    </div>
-</Link>
-{/* Login Option End */}
-
-
+                    </Link>
 
                     {/* Mobile icon start */}
                     <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
